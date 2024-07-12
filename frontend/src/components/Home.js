266ddf8571244162e052bf5493bfe03f2e48caf1
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-07-12 23:21:45 +0530

    Finished the Home component

---
+++ frontend/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/styles.css";
+
+function Home() {
+  return (
+    <section className="home">
+      <div className="home-content">
+        <h1>I'm Imthadh Ahamed</h1>
+        <p>Software Developer & Data Scientist</p>
+        <button>Contact Me</button>
+      </div>
+    </section>
+  );
+}
+
+export default Home;
