793131aa17027aeab2bd8b177cab088f78c103f6
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-07-12 23:18:42 +0530

    Finished the Header componet

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../styles/styles.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">Imthadh Ahamed</div>
+      <nav className="nav">
+        <a href="#about">About</a>
+        <a href="#portfolio">Portfolio</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header
