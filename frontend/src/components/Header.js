ea5cf2820922808618aebd013759b48d250702de
Imthadh Ahamed <143585845+imthadh-ahamed@users.noreply.github.com>
2024-07-13 21:00:00 +0530

    Update

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
 import "../styles/styles.css";
+import { FaBars, FaTimes } from "react-icons/fa"; // Add icons for the menu button
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header className="header">
       <div className="logo">
-        <div class="loading">
+        <div className="loading">
           <span></span>
           <span></span>
           <span></span>
@@ -13,14 +20,25 @@ function Header() {
           <span></span>
         </div>
       </div>
-      <nav className="nav">
-        <a href="#about">About</a>
-        <a href="#portfolio">Portfolio</a>
-        <a href="#experience">Experience</a>
-        <a href="#licences">Licences</a>
+      <nav className={`nav ${isOpen ? "nav-open" : ""}`}>
+        <a href="#about" onClick={() => setIsOpen(false)}>
+          About
+        </a>
+        <a href="#portfolio" onClick={() => setIsOpen(false)}>
+          Portfolio
+        </a>
+        <a href="#experience" onClick={() => setIsOpen(false)}>
+          Experience
+        </a>
+        <a href="#licences" onClick={() => setIsOpen(false)}>
+          Licences
+        </a>
       </nav>
+      <div className="menu-icon" onClick={toggleMenu}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
     </header>
   );
 }
 
-export default Header
+export default Header;
